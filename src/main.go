19d7483903fe95870963ba521b6162ecdfe390e0
xish <tyler.boulware@gmail.com>
2017-08-14 04:31:00 -0500

    Implement .wav FFT correctly with graph output
    Todo: currently, this only works with 16-bit, single-channel WAVs and
    has undefined behavior otherwise

--- src/main.go
+++ src/main.go
@@ -15,21 +15,21 @@ import (
 	"github.com/mjibson/go-dsp/fft"
 
 	"math"
-	"reflect"
+//	"reflect"
 )
 
 func main() {
-	fmt.Println(math.MaxInt32)
-	
-	wavFileName := "data/trumpet/c5(16).wav"
+	//	wavFileName := "data/trumpet/c5(16).wav"
+	wavFileName := "data/sin432(16bit).wav"
 	wavFile, err := os.Open(wavFileName);
 	checkErr(err)
 	wavReader, err := wav.New(wavFile)
 	checkErr(err)
 
-	fmt.Println(wavReader)
 	sampleRate := wavReader.Header.SampleRate
 	sampleCount := wavReader.Samples
+
+	fmt.Println("sample count:", sampleCount)
 
 	wavMagnitudes, err := wavReader.ReadSamples(sampleCount)
 	wavMagnitudesInt16 := wavMagnitudes.([]int16)
@@ -38,16 +38,15 @@ func main() {
 		wavMagnitudesFloat64[i] = float64(wavMagnitudesInt16[i])
 	}
 	
-	fmt.Println(reflect.TypeOf(wavMagnitudesInt16))
-	checkErr(err)
-
-	// complex values of form (time, magnitude) for use in FFT
-	wavComplex := make([]complex128, sampleCount, sampleCount)
+	wavTimeMagnitudeTable := make([]complex128, sampleCount, sampleCount)
 	for i := 0; i < sampleCount; i++ {
-		wavComplex[i] = complex(float64(i) / float64(sampleRate), wavMagnitudesFloat64[i])
+		wavTimeMagnitudeTable[i] = complex(float64(i) / float64(sampleRate), wavMagnitudesFloat64[i])
 	}
 
-	wavFFT := fft.FFT(wavComplex)
+	wavFFT := fft.FFT(wavTimeMagnitudeTable)
+	for ind, val := range wavFFT {
+		wavFFT[ind] = val / complex(float64(sampleCount), 0.0)
+	}
 	
 	plt, err := plot.New()
 	checkErr(err)
@@ -56,26 +55,37 @@ func main() {
 		gran := 1
 		pts := make(plotter.XYs, len(wavData) / gran)
 		for i := 0; i < len(wavData) / gran; i += 1 {
-			val := wavData[i * gran]
+			val := wavData[i]
 			pts[i].X = real(val)
 			pts[i].Y = imag(val)
-			if imag(val) > 1e8 {
-				fmt.Println(real(val), imag(val))
-			}
 		}
 
 		return pts
 	}
 
-	wavXYs := wavToXYs(wavFFT)
 	
-	err = plotutil.AddScatters(plt,
+	wavFreqAmpTable := make([]complex128, sampleCount, sampleCount)
+	for i, val := range wavFFT {
+		x := real(val)
+		y := imag(val)
+		frequency := float64(i) * (float64(sampleRate) / float64(sampleCount))
+		amplitude := math.Sqrt(math.Pow(x, 2.0) + math.Pow(y, 2.0))
+		wavFreqAmpTable[i] = complex(frequency, amplitude)
+		//		if amplitude > 10 { fmt.Println(frequency, amplitude) }
+		if amplitude > 200{
+			fmt.Println(frequency, amplitude)
+		}
+	}
+
+	wavXYs := wavToXYs(wavFreqAmpTable[0 : len(wavFreqAmpTable) / 2])
+	
+	err = plotutil.AddLinePoints(plt,
 		"", wavXYs)
 	checkErr(err)
 
 	fmt.Println("generating plot")
 	outputFileName := "WaveForm.png"
-	err = plt.Save(50*vg.Inch, 4*vg.Inch, outputFileName)
+	err = plt.Save(10*vg.Inch, 10*vg.Inch, outputFileName)
 	checkErr(err)	
 }
 
